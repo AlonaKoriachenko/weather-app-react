8456b1c5e2d238be7a6b8b2a4c3c3898d900ca7f
AlonaKoriachenko <131845750+AlonaKoriachenko@users.noreply.github.com>
2023-06-24 01:07:01 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer></footer>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://cosmic-khapse-25c262.netlify.app/" target="_blank">
+          Alona Koriachenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AlonaKoriachenko/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
