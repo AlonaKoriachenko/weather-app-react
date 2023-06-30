dce49daa13b8e143a4c2cc06c325f3c116c29eed
AlonaKoriachenko <131845750+AlonaKoriachenko@users.noreply.github.com>
2023-06-30 00:48:55 -0700

    Added HTML

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://cosmic-khapse-25c262.netlify.app/" target="_blank">
-          Alona Koriachenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/AlonaKoriachenko/weather-app-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://cosmic-khapse-25c262.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alona Koriachenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AlonaKoriachenko/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
